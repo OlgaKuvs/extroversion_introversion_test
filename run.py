4566ac51b202361fefbca28855c7c7def3024668
OlgaKuvs <olja_kuv@inbox.lv>
2023-05-10 21:19:16 +0100

    Import colorama, termcolor and add user input

--- run.py
+++ run.py
@@ -1,5 +1,43 @@
-# from openpyxl import Workbook
 from openpyxl import load_workbook
-# wb = Workbook()
+from colorama import just_fix_windows_console
+from colorama import Fore, Back, Style
+from colorama import init
+from termcolor import colored, cprint
+
+just_fix_windows_console()
+init(autoreset=True)
+
 wb2 = load_workbook('C:/My websites/Project_3_Test/test_e_i.xlsx')
-print(wb2.sheetnames)
+
+ws = wb2.active
+ws2 = ws['A4'].value
+ws3 = ws['B4'].value
+
+""" 
+print(Fore.RED + 'some red text')
+print(Back.GREEN + 'and with a green background')
+print(Style.DIM + 'and in dim text')
+print(Style.RESET_ALL)
+
+text = colored("Hello, World!", "red", attrs=["reverse", "blink"])
+print(text)
+cprint("Hello, World!", "green", "on_red")
+
+print_red_on_cyan = lambda x: cprint(x, "red", "on_cyan")
+print_red_on_cyan("Hello, World!")
+print_red_on_cyan("Hello, Universe!")
+"""
+
+# print(Fore.RED + Back.YELLOW  + "Welcome to Extroversion Introversion Test")
+cprint(" Welcome to Extroversion Introversion Test! ", "green", "on_red")
+
+name_str = input("Please enter your name:\n")
+# email_str = input("Please enter your email:\n")
+if name_str.isalpha():
+    print(f"Hello, {name_str}\n")
+else:
+     print(f"Incorrect input... Please enter correct name: \n")
+
+
+
+
